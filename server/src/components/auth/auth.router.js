ca5278c1e6f0b496a9f9fd3e2a23173ffa488001
Mirian Okradze <okradzemirian@gmail.com>
2020-02-22 21:26:42 +0400

    added login to signup user

--- server/src/components/auth/auth.router.js
+++ server/src/components/auth/auth.router.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const db = require('../../../database/models');
 
 module.exports = (apiRouter) => {
-  router.post('/signup', (req, res) => {
-    res.json({ message: 'Hello' });
+  router.post('/signup', async (req, res) => {
+    const user = await db.User.create(req.body);
+    res.json(user);
   });
 
   apiRouter.use('/v1/auth', router);
