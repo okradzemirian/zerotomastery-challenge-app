044d620655f835005ccab74b7106203dc86d9273
Mirian Okradze <okradzemirian@gmail.com>
2020-02-22 21:33:50 +0400

    added bcrypt to hash password on signup

--- server/src/components/auth/auth.router.js
+++ server/src/components/auth/auth.router.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const db = require('../../../database/models');
 
 module.exports = (apiRouter) => {
   router.post('/signup', async (req, res) => {
     try {
-      const user = await db.User.create(req.body);
+      const password = await bcrypt.hash(req.body.password, 10);
+      const user = await db.User.create({ ...req.body, password });
       res.json(user);
     } catch (error) {
       res.send('Unable to signup');
